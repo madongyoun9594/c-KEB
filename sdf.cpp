b50367f8314abd5f37f97228b33efb7088d9f43b
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-26 17:09:11 +0900

    v0.3 KEB c++

--- sdf.cpp
+++ sdf.cpp
@@ -4,26 +4,47 @@ using namespace std;
 
 int main()
 {
-    // 변수설정
-    int N_1;
-    int N_2;
+    //변수설정
+    int age;
+    int license;
+    bool possible;
 
-    cout << "숫자를 입력하세요: ";
-    cin >> N_1;
-    cout << "숫자를 입력하세요: ";
-    cin >> N_2;
+    while (license == 1)
+    { 
+        cout << "면허가 있으면 1번 없으면 2번을 입력하세요: ";
+        cin >> license;
+        if(license == 1)
+            {
+            cout << "나이를 입력하세요 : ";
+            cin >> age;
+            
+            possible = (age >= 20) && (age <= 80);
 
+                if(possible)
+                    {
+                        cout << "대여가 가능합니다.";
+                        break;
+                    }
+                    
+                
+                else
+                    {
+                        cout << "대여가 불가능합니다.";
+                        break;
+                    }
+                 
+            }         
+        else
+            {
+                cout << "자격이 안됩니다.";
+                break;
+            }
+               
+        
+    }
+    return 0;
 
-    if(N_1 > N_2)
-        { 
-            cout << "두 수중 더 큰 수는" << N_1 << "입니다";
-        }    
-    else
-        {
-            cout << "두 수중 더 큰 수는" << N_2 << "입니다";
-        } 
-
-    return 0;    
+  
 
 
 
