4e850abb443f61e419f932032a4af9a54d06c1d7
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-26 18:41:40 +0900

    v0.5 KEB c++

--- sdf.cpp
+++ sdf.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
-#include <limits>
 using namespace std;
 
 int main()
 {
-    long long num1 = numeric_limits<long long>::max();
-    long long num2 = numeric_limits<long long>::min();
-
-
-    cout << "long long number의 최댓값: " << num1 << endl;
-    cout << "long long number의 최솟값: " << num2 << endl;
-        
+    int menu;
     
+    while(true)
+    
+        {
+            cout <<  "1) 시작  2) 종료 : ";
+            cin >> menu;
+    
+            if(menu == 1)
+            {
+                cout << "start" << endl;
+                
+            }
+            else if(menu == 2)
+            {
+                cout << "terminate" << endl;
+                break;
+            }
+            
+        }
+
     return 0;
 
 }
