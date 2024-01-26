e2c2fc6727e7ac9db68907b80bbab49d1e4e6641
madongyoun9594 <madongyoun9594@gmail.com>
2024-01-26 16:30:27 +0900

    v0.2 KEB c++

--- sdf.cpp
+++ sdf.cpp
@@ -5,18 +5,29 @@ using namespace std;
 int main()
 {
     // 변수설정
-    int T_S;
-    // 입력
-    cout << "점수를 입력하세요.: ";
-    cin >> T_S;
+    int N_1;
+    int N_2;
 
-    if(T_S >= 80)
-        cout << "합격 입니다.";
-    if(T_S < 80)
-        cout << "불합격 입니다.";
+    cout << "숫자를 입력하세요: ";
+    cin >> N_1;
+    cout << "숫자를 입력하세요: ";
+    cin >> N_2;
+
+
+    if(N_1 > N_2)
+        { 
+            cout << "두 수중 더 큰 수는" << N_1 << "입니다";
+        }    
+    else
+        {
+            cout << "두 수중 더 큰 수는" << N_2 << "입니다";
+        } 
+
+    return 0;    
+
+
+
     
-    return 0;
-
-
 }
+
 
